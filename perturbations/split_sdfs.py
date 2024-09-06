94f0fb71f14b2afc0e21341fe4f4b3c8599ad9d0
David-Araripe <david.araripe17@gmail.com>
2024-09-06 15:50:35 +0200

    add script to split sdf files

---
+++ perturbations/split_sdfs.py
@@ -0,0 +1,10 @@
+"""Script to separate sdf files into individual files containing the ligands."""
+
+from pathlib import Path
+from QligFEP.chemIO import MoleculeIO
+
+directories = [d for d in Path().glob("*/") if d.is_dir()]
+for _dir in directories:
+    ligand_path = _dir / "ligands.sdf"
+    ligsIO = MoleculeIO(ligand_path)  # save mols in separate sfd files
+    ligsIO.write_sdf_separate(_dir)
