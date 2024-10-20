227d8dd9007e7b8f11fa1d499538ab562ac80631
David-Araripe <david.araripe17@gmail.com>
2024-10-20 12:15:52 +0200

    add newline to thrombin/protein.pdb & exception handling on split_sdfs.py

--- perturbations/split_sdfs.py
+++ perturbations/split_sdfs.py
@@ -6,5 +6,8 @@ from QligFEP.chemIO import MoleculeIO
 directories = [d for d in Path().glob("*/") if d.is_dir()]
 for _dir in directories:
     ligand_path = _dir / "ligands.sdf"
-    ligsIO = MoleculeIO(ligand_path)  # save mols in separate sfd files
-    ligsIO.write_sdf_separate(_dir)
+    try:
+        ligsIO = MoleculeIO(ligand_path)  # save mols in separate sfd files
+        ligsIO.write_sdf_separate(_dir)
+    except ValueError:
+        print(f'no ligands for {ligand_path}. Continuing...')
